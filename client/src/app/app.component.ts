3c6284201ff0d13cc4a2beed653a8f4b4446aa95
SheroukRashed <eng_sheroukrashed@yahoo.com>
2022-01-26 18:35:46 +0200

    Add Nav bar login sessions

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { User } from './_models/user';
+import { AccountService } from './_services/account.service';
 
 @Component({
   selector: 'app-root',
@@ -11,10 +13,11 @@ export class AppComponent implements OnInit{
   title = 'Dating App';
   users: any;
 
-  constructor(private http : HttpClient){}
+  constructor(private http : HttpClient, private accountService: AccountService){}
 
   ngOnInit() {
       this.getUsers();
+      this.setCurrentUser();
   }
 
   getUsers() {
@@ -28,4 +31,12 @@ export class AppComponent implements OnInit{
       complete: () => console.info('complete') 
     });
   }
+
+  setCurrentUser() {
+    const user: User = JSON.parse(localStorage.getItem('user')  || '{}');
+    if (user.userName && user.token) {
+      this.accountService.setCurrentUser(user);
+    }
+  }
+
 }
---
+++ client/src/app/nav/nav.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from '../_models/user';
+import { AccountService } from '../_services/account.service';
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.css']
+})
+export class NavComponent implements OnInit {
+
+  model: any = {};
+  currentUser$: Observable<User|null>;
+
+  constructor(private accountService: AccountService) { 
+    this.currentUser$ = this.accountService.currentUser$;
+  }
+
+  ngOnInit(): void {
+  }
+
+  login() {
+    this.accountService.login(this.model).subscribe({
+      next: (response) => {
+        console.log(response);
+      },
+      error: (error) => {
+        console.log(error);
+      },
+      complete: () => console.info('complete') 
+    });;
+  }
+
+  logout(){
+    this.accountService.logout();
+  }
+
+}
